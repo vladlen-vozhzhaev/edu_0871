fefc9cbf4f077c3c3473c2c077c73fdcd916fe7d
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-08-18 15:27:17 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,10 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
