f3f447516fcd50059fd75b98210c79ef1d776c93
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-08-21 12:01:16 +0300

    Create Main.java

---
+++ 20.08.2020/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        float nums[] = {4,6,5,9,8,45,2,36,8,4,3};
+//        for (int i=0; i<nums.length; i++)
+//            System.out.println("Index: "+i+" || Array value: "+nums[i]);
+//        int max = 0;
+//        for (int i=0; i<nums.length; i++){
+//            if (max<nums[i] && nums[i]%2==0)
+//                max = nums[i];
+//        }
+//        System.out.println("Максимальное число в массиве nums: "+max);
+//        float sum = 0;
+//        for (int i=0; i<nums.length; i++){
+//            sum += nums[i]; // sum = sum + nums[i];
+//        }
+//        System.out.println(sum/nums.length);
+//        for (int i=1; i<nums.length; i++) nums[0]+=nums[i];
+//        System.out.println(nums[0]/nums.length);
+    }
+}
