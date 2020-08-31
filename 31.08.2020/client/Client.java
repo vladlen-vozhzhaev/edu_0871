6ca9c464abb56dd7cda440db9533bf752e98de09
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2020-08-31 15:22:45 +0300

    Create Client.java

---
+++ 31.08.2020/client/Client.java
@@ -0,0 +1,39 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost",8189);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+            Thread responseThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true){
+                        String response = null;
+                        try {
+                            response = in.readUTF();
+                            System.out.println(response);
+                        } catch (IOException exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                }
+            });
+            responseThread.start();
+            while (true){
+                String str = scanner.nextLine();
+                out.writeUTF(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
